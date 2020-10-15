8e6f240266f835707638b95edfd12198f254a5b2
Kait Low <kdlowl@gmail.com>
2020-10-15 14:55:12 -0600

    Added collision controller, disabled controls on collision.

---
+++ 4_Argon_Assault/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Player triggered an event");
+        StartDeathSequence();
+    }
+
+    private void StartDeathSequence()
+    {
+        print("Player dying");
+        SendMessage("OnPlayerDeath");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("Player hit something");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- 4_Argon_Assault/Assets/Scripts/PlayerController.cs
+++ 4_Argon_Assault/Assets/Scripts/PlayerController.cs
@@ -5,33 +5,42 @@ using UnityStandardAssets;
 using UnityStandardAssets.CrossPlatformInput;
 
 // test commit comment
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    [Header("General")]
     [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f; //default 4 meters
     [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 4f; //default 4 meters
     [Tooltip("In meters")][SerializeField] const float xRange = 6.01f;  // limit range of ship
     [Tooltip("In meters")] [SerializeField] const float yMin = 3.97f;  // limit range of ship
     [Tooltip("In meters")] [SerializeField] const float yMax = 3.97f;  // limit range of ship
 
+    [Header("Screen-position Based")]
     [SerializeField] float positionPitchFactor = -6.5f;
     [SerializeField] float controlPitchFactor = -6.5f;
+    [Header("Control-throw Based")]
     [SerializeField] float positionYawFactor = 6.5f;
     [SerializeField] float controlYawFactor = 6.5f;
     [SerializeField] float controlRollFactor = -24f;
     float xThrow, yThrow;  // need to access this infor from other locations.
+    bool isControlEnabled = true;
 
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     void Update()
     {
-        ProcessTranslation();  // takes place before ProcessRotation() to set Throw
-        ProcessRotation();
+        if (isControlEnabled)
+        {
+            ProcessTranslation();  // takes place before ProcessRotation() to set Throw
+            ProcessRotation();
+        }
     }
-
+    void OnPlayerDeath()  //called by string reference
+    {
+        isControlEnabled = false;
+    }
     void ProcessRotation()
     {
         float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
