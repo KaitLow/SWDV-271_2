3f4967cdf1055f4bf508044ea4fbd5bc093042aa
Kait Low <kdlowl@gmail.com>
2020-10-15 16:23:44 -0600

    Added Enemy script.

--- 4_Argon_Assault/Assets/Scripts/CollisionHandler.cs
+++ 4_Argon_Assault/Assets/Scripts/CollisionHandler.cs
@@ -2,9 +2,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [Tooltip("In seconds")][SerializeField] float loadLevelDelay = 1f;
+    [Tooltip("FX prefab on player")] [SerializeField] GameObject deathFX;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,13 +17,17 @@ public class CollisionHandler : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        print("Player triggered an event");
         StartDeathSequence();
+        deathFX.SetActive(true);
+        Invoke("ReloadScene", loadLevelDelay); 
+    }
+    private void ReloadScene()  // String referenced
+    {
+        SceneManager.LoadScene(1);
     }
 
     private void StartDeathSequence()
     {
-        print("Player dying");
         SendMessage("OnPlayerDeath");
     }
 
