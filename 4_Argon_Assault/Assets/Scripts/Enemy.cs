c142bbfa46a946c37b2b58af7d9dca41304d2f50
Kait Low <kdlowl@gmail.com>
2020-10-20 13:22:44 -0600

    Updated scoring to allow each ship to have its own hit score.

--- 4_Argon_Assault/Assets/Scripts/Enemy.cs
+++ 4_Argon_Assault/Assets/Scripts/Enemy.cs
@@ -7,11 +7,15 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathFX; // deaths effect for enemy ships
     [SerializeField] Transform parent;  // placeholder to destroy deathFX when done
+    [SerializeField] int scoreIncrease = 12;
+
+    ScoreBoard scoreBoard;
 
     // Start is called before the first frame update
     void Start()
     {
         AddNonTriggerBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
     }
 
     private void AddNonTriggerBoxCollider()
@@ -30,8 +34,7 @@ public class Enemy : MonoBehaviour
     {
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
         fx.transform.parent = parent;
-        ScoreBoard scoreBoard = FindObjectOfType<ScoreBoard>();
-        scoreBoard.ScoreHit();
+        scoreBoard.ScoreHit(scoreIncrease);
 
         print("Particles collided with enemy" + gameObject.name);
         Destroy(gameObject);
