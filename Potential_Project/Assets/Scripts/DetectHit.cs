5b4a05f8128eaf7922f11f5c490fdf713f15650f
Kait Low <kdlowl@gmail.com>
2020-10-29 10:11:19 -0600

    Added enemy movement toward player. Once in range, displays attacked message.

---
+++ Potential_Project/Assets/Scripts/DetectHit.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DetectHit : MonoBehaviour
+{
+    public Slider healthbar;
+    Animator anim;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        healthbar.value -= 20;
+        
+        if (healthbar.value <= 0)
+        {
+            anim.SetBool("die", true);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Potential_Project/Assets/Scripts/EnemyAi.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class EnemyAi : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 50f;
+
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
+
+    //public Slider healthbar;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        //if (healthbar.value <= 0) return;
+
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (isProvoked)
+        {
+            EngageTarget();
+        }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;         
+        }       
+    }
+    private void EngageTarget()
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+    private void ChaseTarget()
+    { 
+        navMeshAgent.SetDestination(target.position);
+    }
+    private void AttackTarget()
+    {
+        Debug.Log("has attacked");
+    }
+
+}
